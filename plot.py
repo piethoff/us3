1798ed444bb36e1cac51fd442e60fdcbc1b0ae01
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2018-05-12 16:31:06 +0200

    durchfuehrung hinzugefuegt

--- plot.py
+++ plot.py
@@ -1,21 +1,48 @@
+import matplotlib as mpl
+
+mpl.use('pgf')
+mpl.rcParams.update({
+    'pgf.preamble': r'\usepackage{siunitx}',
+})
+
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+from uncertainties import unumpy
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+#plot1
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+data = np.genfromtxt('content/15.txt', unpack=True)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+x = data[0]
+y = data[1]
+
+
+plt.xlabel(r'v$/RPM$')
+plt.ylabel(r'$\Delta \nu /\cos(\alpha)$')
+
+plt.grid(True, which='both')
+
+
+# Fitvorschrift
+def f(x, A, B, C):
+    return A*x**2+ B*x+ C
+
+
+params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
+uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+for i in range(0, len(uparams)):
+    print(chr(ord('A') + i), "=" , uparams[i])
+print()
+
+lin = np.linspace(x[0], x[-1], 1000)
+plt.plot(lin, f(lin, *params), "xkcd:orange", label=r'Röhrendurchmesser=$10\si{\milli\meter}$' )
+plt.plot(x, y, ".", color="xkcd:blue", label="Messwerte")
+
+
+
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/15.pdf')
+plt.clf()
