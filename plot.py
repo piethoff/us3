0d002297adfd2443b63892da04cf7c4f52a52335
Piet Hoffmann <hoffmannpiet42@gmail.com>
2018-05-14 00:37:31 +0200

    auswertungsteil 1 korregiert, soweit möglich. schriftbild optimiert

--- plot.py
+++ plot.py
@@ -9,226 +9,132 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import unumpy
 
-#plot1
+c_l = 1800
+c_p = 2700
 
-data = np.genfromtxt('content/15.txt', unpack=True)
-data1 = np.genfromtxt('content/30.txt', unpack=True)
-data2 = np.genfromtxt('content/60.txt', unpack=True)
-
-
-x = (-1)*1800*(data[0])/(2*2000000*np.cos(80.06))
-y = (-1)*data[0]/np.cos(80.06)
-
-s =(-1)*1800*(data1[1])/(2*2000000*np.cos(70.53))
-t =(-1)*data1[1]/np.cos(70.53)
-
-q =1800*(data2[1])/(2*2000000*np.cos(54.74))
-r =data2[1]/np.cos(54.74)
-
-
-plt.xlabel(r'v$/\si{\meter\per\second}$')
-plt.ylabel(r'$\Delta \nu /\cos(\alpha)$')
-
-plt.grid(True, which='both')
-
-
-# Fitvorschrift
 def f(x, A, B):
     return A*x + B
 
 
-params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('A') + i), "=" , uparams[i])
-print()
+##15°##
+data1 = np.genfromtxt("content/15.txt", unpack=True)
 
-lin = np.linspace(x[0], x[-1], 1000)
-plt.plot(lin, f(lin, *params), "xkcd:orange", label=r'$\theta = 15\si{\degree}$' )
+data2 = np.append(data1[0], data1[2])
+data2 = np.append(data2, data1[4])
+data = data2
 
-# Fit 2
-def g(s, S, T):
-    return S*s + T
-
-
-params, covar = curve_fit(g, s, t)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('S') + i), "=" , uparams[i])
-print()
-
-lin = np.linspace(s[0], s[-1], 1000)
-plt.plot(lin, g(lin, *params), "xkcd:red", label=r'$\theta = 30\si{\degree}$' )
+data2 = np.append(data1[1], data1[3])
+data2 = np.append(data2, data1[5])
+data = np.array([data, data2])
+data[1] = np.abs(data[1])
 
 
-# Fit 3
-def h(q, Q, R):
-    return Q*q + R
+#for i in range(data[0].size):
+#	print(data[0][i], data[1][i], sep="\t& ", end="\\\\\n")
 
+data[1] /= np.cos(np.pi/2 - np.arcsin(np.sin(2*np.pi/360 * 15) * c_l/c_p))
 
-params, covar = curve_fit(h, q, r)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('Q') + i), "=" , uparams[i])
-print()
+print("15°:\n")
+for i in range(0,3):
+	plt.plot(data[0][5*i:5*(i+1)], data[1][5*i:5*(i+1)], ".", label="Messwerte")
+	params, covar = curve_fit(f, data[0][5*i:5*(i+1)], data[1][5*i:5*(i+1)])
+	uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+	print(i+1, "-tes Rohr:", sep="")
+	for j in range(0, len(uparams)):
+		print(chr(ord('A') + j), "=" , uparams[j])
+	print()
+	lin = np.linspace(data[0][5*i], data[0][5*(i+1)-1], 10)
+	plt.plot(lin, f(lin, *params), color="xkcd:orange", label="lin. Fit")
 
-lin = np.linspace(q[0], q[-1], 1000)
-plt.plot(lin, h(lin, *params), "xkcd:green", label=r'$\theta = 60\si{\degree}$' )
-
-plt.plot(x, y, "x", color="xkcd:blue", label="Messwerte($15\si{\degree}$)")
-plt.plot(s, t, "x", color="xkcd:red", label="Messwerte($30\si{\degree}$)")
-plt.plot(q, r, "x", color="xkcd:green", label="Messwerte($60\si{\degree}$)")
-
-plt.tight_layout()
 plt.legend()
-plt.savefig('build/15.pdf')
+plt.xlabel("Strömungsgeschwindigkeit/RPM")
+plt.ylabel(r"$\frac{\Delta\nu}{\cos\alpha}/\si{\hertz}$")
+plt.tight_layout()
+plt.savefig("build/15.pdf")
 plt.clf()
 
 
-#plot2
-data = np.genfromtxt('content/15.txt', unpack=True)
-data1 = np.genfromtxt('content/30.txt', unpack=True)
-data2 = np.genfromtxt('content/60.txt', unpack=True)
 
 
-x = 1800*(data[3])/(2*2000000*np.cos(80.06))
-y = data[3]/np.cos(80.06)
-
-s =(-1)*1800*(data1[3])/(2*2000000*np.cos(70.53))
-t =(-1)*data1[3]/np.cos(70.53)
-
-q =1800*(data2[3])/(2*2000000*np.cos(54.74))
-r =data2[3]/np.cos(54.74)
 
 
-plt.xlabel(r'v$/\si{\meter\per\second}$')
-plt.ylabel(r'$\Delta \nu /\cos(\alpha)$')
-
-plt.grid(True, which='both')
 
 
-# Fitvorschrift
-def f(x, A, B):
-    return A*x + B
+##30°##
+data1 = np.genfromtxt("content/30.txt", unpack=True)
+
+data2 = np.append(data1[0], data1[2])
+data2 = np.append(data2, data1[4])
+data = data2
+
+data2 = np.append(data1[1], data1[3])
+data2 = np.append(data2, data1[5])
+data = np.array([data, data2])
+data[1] = np.abs(data[1])
 
 
-params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('A') + i), "=" , uparams[i])
-print()
+#for i in range(data[0].size):
+#	print(data[0][i], data[1][i], sep="\t& ", end="\\\\\n")
 
-lin = np.linspace(x[0], x[-1], 1000)
-plt.plot(lin, f(lin, *params), "xkcd:orange", label=r'$\theta = 15\si{\degree}$' )
+data[1] /= np.cos(np.pi/2 - np.arcsin(np.sin(2*np.pi/360 * 30) * c_l/c_p))
 
-# Fit 2
-def g(s, S, T):
-    return S*s + T
+print("30°:\n")
+for i in range(0,3):
+	plt.plot(data[0][5*i:5*(i+1)], data[1][5*i:5*(i+1)], ".", label="Messwerte")
+	params, covar = curve_fit(f, data[0][5*i:5*(i+1)], data[1][5*i:5*(i+1)])
+	uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+	print(i+1, "-tes Rohr:", sep="")
+	for j in range(0, len(uparams)):
+		print(chr(ord('A') + j), "=" , uparams[j])
+	print()
+	lin = np.linspace(data[0][5*i], data[0][5*(i+1)-1], 10)
+	plt.plot(lin, f(lin, *params), color="xkcd:orange", label="lin. Fit")
 
-
-params, covar = curve_fit(g, s, t)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('S') + i), "=" , uparams[i])
-print()
-
-lin = np.linspace(s[0], s[-1], 1000)
-plt.plot(lin, g(lin, *params), "xkcd:red", label=r'$\theta = 30\si{\degree}$' )
-
-
-# Fit 3
-def h(q, Q, R):
-    return Q*q + R
-
-
-params, covar = curve_fit(h, q, r)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('Q') + i), "=" , uparams[i])
-print()
-
-lin = np.linspace(q[0], q[-1], 1000)
-plt.plot(lin, h(lin, *params), "xkcd:green", label=r'$\theta = 60\si{\degree}$' )
-
-plt.plot(x, y, "x", color="xkcd:blue", label="Messwerte($15\si{\degree}$)")
-plt.plot(s, t, "x", color="xkcd:red", label="Messwerte($30\si{\degree}$)")
-plt.plot(q, r, "x", color="xkcd:green", label="Messwerte($60\si{\degree}$)")
-
-plt.tight_layout()
 plt.legend()
-plt.savefig('build/30.pdf')
+plt.xlabel("Strömungsgeschwindigkeit/RPM")
+plt.ylabel(r"$\frac{\Delta\nu}{\cos\alpha}/\si{\hertz}$")
+plt.tight_layout()
+plt.savefig("build/30.pdf")
 plt.clf()
-#plot3
-
-data = np.genfromtxt('content/15.txt', unpack=True)
-data1 = np.genfromtxt('content/30.txt', unpack=True)
-data2 = np.genfromtxt('content/60.txt', unpack=True)
 
 
-x = 1800*(data[5])/(2*2000000*np.cos(80.06))
-y = data[5]/np.cos(80.06)
-
-s =(-1)*1800*(data1[5])/(2*2000000*np.cos(70.53))
-t =(-1)*data1[5]/np.cos(70.53)
-
-q =1800*(data2[5])/(2*2000000*np.cos(54.74))
-r =data2[5]/np.cos(54.74)
 
 
-plt.xlabel(r'v$/\si{\meter\per\second}$')
-plt.ylabel(r'$\Delta \nu /\cos(\alpha)$')
-
-plt.grid(True, which='both')
 
 
-# Fitvorschrift
-def f(x, A, B):
-    return A*x + B
+##60°##
+data1 = np.genfromtxt("content/60.txt", unpack=True)
+
+data2 = np.append(data1[0], data1[2])
+data2 = np.append(data2, data1[4])
+data = data2
+
+data2 = np.append(data1[1], data1[3])
+data2 = np.append(data2, data1[5])
+data = np.array([data, data2])
+data[1] = np.abs(data[1])
 
 
-params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('A') + i), "=" , uparams[i])
-print()
+#for i in range(data[0].size):
+#	print(data[0][i], data[1][i], sep="\t& ", end="\\\\\n")
 
-lin = np.linspace(x[0], x[-1], 1000)
-plt.plot(lin, f(lin, *params), "xkcd:orange", label=r'$\theta = 15\si{\degree}$' )
+data[1] /= np.cos(np.pi/2 - np.arcsin(np.sin(2*np.pi/360 * 60) * c_l/c_p))
 
-# Fit 2
-def g(s, S, T):
-    return S*s + T
+print("60°:\n")
+for i in range(0,3):
+	plt.plot(data[0][5*i:5*(i+1)], data[1][5*i:5*(i+1)], ".", label="Messwerte")
+	params, covar = curve_fit(f, data[0][5*i:5*(i+1)], data[1][5*i:5*(i+1)])
+	uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+	print(i+1, "-tes Rohr:", sep="")
+	for j in range(0, len(uparams)):
+		print(chr(ord('A') + j), "=" , uparams[j])
+	print()
+	lin = np.linspace(data[0][5*i], data[0][5*(i+1)-1], 10)
+	plt.plot(lin, f(lin, *params), color="xkcd:orange", label="lin. Fit")
 
-
-params, covar = curve_fit(g, s, t)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('S') + i), "=" , uparams[i])
-print()
-
-lin = np.linspace(s[0], s[-1], 1000)
-plt.plot(lin, g(lin, *params), "xkcd:red", label=r'$\theta = 30\si{\degree}$' )
-
-
-# Fit 3
-def h(q, Q, R):
-    return Q*q + R
-
-
-params, covar = curve_fit(h, q, r)            #eigene Messwerte hier uebergeben
-uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
-for i in range(0, len(uparams)):
-    print(chr(ord('Q') + i), "=" , uparams[i])
-print()
-
-lin = np.linspace(q[0], q[-1], 1000)
-plt.plot(lin, h(lin, *params), "xkcd:green", label=r'$\theta = 60\si{\degree}$' )
-
-plt.plot(x, y, "x", color="xkcd:blue", label="Messwerte($15\si{\degree}$)")
-plt.plot(s, t, "x", color="xkcd:red", label="Messwerte($30\si{\degree}$)")
-plt.plot(q, r, "x", color="xkcd:green", label="Messwerte($60\si{\degree}$)")
-
-plt.tight_layout()
 plt.legend()
-plt.savefig('build/60.pdf')
+plt.xlabel("Strömungsgeschwindigkeit/RPM")
+plt.ylabel(r"$\frac{\Delta\nu}{\cos\alpha}/\si{\hertz}$")
+plt.tight_layout()
+plt.savefig("build/60.pdf")
 plt.clf()
