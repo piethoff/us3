2cbbd5c2418a38d0afc6d00a2d145016c377dd1c
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2018-05-13 18:59:12 +0200

    kleine Korrektur der Werte

--- plot.py
+++ plot.py
@@ -15,9 +15,9 @@ from uncertainties import unumpy
 data = np.genfromtxt('content/15.txt', unpack=True)
 
 
-x = 1800*(-data[1])/(2*2000*np.cos(80.06)
+x = 1800*(data[0])/(2*2000000*np.cos(80.06)
 
-y = data[1]/np.cos(80.06)
+y = (-1)*data[1]/np.cos(80.06)
 
 plt.xlabel(r'v$/\si{\meter\per\second}$')
 plt.ylabel(r'$\Delta \nu /\cos(\alpha)$')
@@ -27,7 +27,7 @@ plt.grid(True, which='both')
 
 # Fitvorschrift
 def f(x, A, B):
-    return A*x*+ B
+    return A*x + B
 
 
 params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
